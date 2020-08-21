61c6bb9013dd14f21fcd61055daee92c6d74fad0
Hosmel Quintana <hosmelq@gmail.com>
2020-08-21 15:18:18 -0600

    init

---
+++ app/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}

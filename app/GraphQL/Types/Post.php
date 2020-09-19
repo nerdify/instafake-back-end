fa404bbe923f9b73be1fdd2ae0b3568a1761d1af
sald19 <carl.sald19@gmail.com>
2020-09-19 10:07:27 -0600

    show if viewer has saved current post

--- app/GraphQL/Types/Post.php
+++ app/GraphQL/Types/Post.php
@@ -8,6 +8,20 @@ use Nuwave\Lighthouse\Schema\Context;
 
 class Post
 {
+    public function viewerHasBookmarked(PostModel $post, array $args, Context $context): bool
+    {
+        /** @var User $user */
+        $user = $context->user();
+
+        if (! $user) {
+            return false;
+        }
+
+        return $user->bookmarks()
+            ->where('post_id', $post->id)
+            ->exists();
+    }
+
     public function viewerHasLiked(PostModel $post, array $args, Context $context): bool
     {
         /** @var User $user */
